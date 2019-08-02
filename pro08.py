1e4891140601f469ffe25aef8ed3c6047b03b74c
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-08-02 14:45:55 +0530

    Update pro08.py

--- pro08.py
+++ pro08.py
@@ -1,13 +1,14 @@
 # pro08.py
 import math
-viji,lak=map(int,input().split())
-sug=[]
-ttt=list(map(int,input().split()))
-for i in range(0,lak):
-    lac,hac=map(int,input().split())
-    sug.append([lac,hac])
-for i in sug:
-    den=i[0]-1
-    eat=i[1]-1
-    print(math.gcd(ttt[den],ttt[eat]))
+bala,vg=map(int,input().split())
+sup=[]
+tsp=list(map(int,input().split()))
+for i in range(0,vg):
+    but,hlg=map(int,input().split())
+    sup.append([but,hlg])
+for i in sup:
+    gg=i[0]-1
+    ate=i[1]-1
+    print(math.gcd(tsp[gg],tsp[ate]))
 
+    
